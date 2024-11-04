2de296491c62a8a4b58a068e2b1f98ff74604a3a
Victor Luc Etienne Viterbo <vviterbo@c1r16s7.42lausanne.ch>
2024-11-04 13:09:21 +0100

    going to remove debug printout to set to norm

--- exit.c
+++ exit.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 15:33:13 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/11/03 18:11:56 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/11/04 11:57:14 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	exit_gracefully(t_list **stack_a, int status);
 void	exit_gracefully(t_list **stack_a, int status)
 {
 	ft_lstclear(stack_a, free);
-	if (status==EXIT_FAILURE)
+	if (status == EXIT_FAILURE)
 		write(1, "Error\n", 6);
 	exit(EXIT_FAILURE);
 }
