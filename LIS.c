fe09f7645a157325d69ef1f15cfc485ceaab1ddc
vviterbo <victor.viterbo@epfl.ch>
2024-11-15 10:12:42 +0100

    making progress

--- LIS.c
+++ LIS.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:44:12 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/11/14 17:46:43 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/11/15 10:12:26 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,28 +40,20 @@ t_list	*get_lis(t_list **stack_a, int *len)
 
 void	do_move(t_list **stack_a, t_list **stack_b)
 {
-	int	i;
-	int	cost;
-	int	min_cost;
-	int	best_i;
-	int	rcase;
+	int		i;
+	int		best_i;
 
 	i = 0;
-	min_cost = -1;
+	best_i = 0;
 	while (i < ft_lstsize(*stack_b))
 	{
-		if (compute_cost(stack_a, stack_b, i) < min_cost)
-		{
-			min_cost = compute_cost(stack_a, stack_b, i);
+		if (ft_abs(compute_cost(stack_a, stack_b, i))
+			< ft_abs(compute_cost(stack_a, stack_b, best_i)))
 			best_i = i;
-		}
 		i++;
 	}
-	i = -1;
-	while (get_n(stack_a, i + 1) < get_n(stack_b, best_i))
-		i++;
-	rcase = get_rcase(stack_a, stack_b, i, best_i);
-	if (rcase == 0)
+	i = 0;
+	if (ft_abs(compute_cost(stack_a, stack_b, best_i)) < 0)
 	{
 		
 	}
@@ -69,32 +61,54 @@ void	do_move(t_list **stack_a, t_list **stack_b)
 
 int	compute_cost(t_list **stack_a, t_list **stack_b, int pos)
 {
-	int	cost;
 	int	i;
-	int	rcase;
+	int forward;
+	int	backward;
+	int	zigzag_forward;
+	int	zigzag_backward;
+	int	min;
 
 	i = 0;
-	while (get_n(stack_a, i + 1) < get_n(stack_b, pos))
+	while (get_n(stack_a, i + 1) < get_n(stack_b, pos)
+		&& i + 1 < ft_lstsize(*stack_a))
 		i++;
-	rcase = get_rcase(stack_a, stack_b, i, pos);
-	if (rcase == 0)
-		return (ft_max(i, pos));
-	else if (rcase == 1)
-		return (ft_max(i, ft_lstsize(*stack_b) - pos));
-	else if (rcase == 2)
-		return (ft_max(ft_listsize(*stack_a) - i, pos));
+	forward = t_max(i, pos);
+	backward = ft_max(ft_lstsize(*stack_a) - i, ft_lstsize(*stack_b) - pos);
+	zigzag_forward = 2 * pos + (ft_lstsize(*stack_a) - i);
+	zigzag_backward = 2 * (ft_lstsize(*stack_b) - pos) + i;
+	min = ft_min(ft_min(forward, backward), ft_min(zigzag_forward, zigzag_backward));
+	if (min == forward || min == zigzag_forward)
+		return (min);
+	else if (min == backward || min == zigzag_backward)
+		return (-min);
 	else
-		return (ft_max(ft_listsize(*stack_a) - i, ft_lstsize(*stack_b) - pos));
+		exit_gracefully(NULL, NULL, EXIT_FAILURE);
+	return (0);
 }
 
-int	get_rcase(t_list **stack_a, t_list **stack_b, int i, int pos)
+void	smart_rotate(t_list **stack_a, t_list **stack_b, int best_i)
 {
-	if (2 * i < ft_lstsize(*stack_a) && 2 * pos < ft_lstsize(*stack_b))
-		return (0);
-	else if (2 * i <= ft_lstsize(*stack_a) && 2 * pos >= ft_lstsize(*stack_b))
-		return (1);
-	else if (2 * i >= ft_lstsize(*stack_a) && 2 * pos <= ft_lstsize(*stack_b))
-		return (2);
-	else
-		return (3);
+	int		i;
+	int		n;
+	bool	rev;
+
+	i = 0;
+	n = compute_cost(stack_a, stack_b, best_i);
+	rev = n < 0;
+	n = ft_abs(n);
+	while (!rev && get_n(stack_a, i + 1) < get_n(stack_b, best_i) && best_i)
+	{
+		rr(stack_a, stack_b);
+		best_i--;
+		i++;
+	}
+	while (rev && get_n(stack_a, i) > get_n(stack_b, best_i) && best_i)
+	{
+		rrr(stack_a, stack_b);
+		best_i = (best_i + 1) % ft_lstsize(*stack_b);
+		i++;
+	}
+	rotate_i(stack_a, stack_b, best_i, 'b');
+	rotate_i(stack_a, stack_b, n - best_i, 'a');
+	return ;
 }
--- rotate.c
+++ rotate.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:58:46 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/11/14 15:41:44 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/11/15 00:11:14 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	rotate(t_list **stack);
 void	ra(t_list **stack_a);
 void	rb(t_list **stack_b);
 void	rr(t_list **stack_a, t_list **stack_b);
-void	rotate_i(t_list **stack, int n, char ab);
+void	rotate_i(t_list **stack_1, t_list **stack_2, int n, char ab);
 
 void	rotate(t_list **stack)
 {
@@ -55,21 +55,23 @@ void	rr(t_list **stack_a, t_list **stack_b)
 	return ;
 }
 
-void	rotate_i(t_list **stack, int n, char ab)
+void	rotate_i(t_list **stack_1, t_list **stack_2, int n, char ab)
 {
 	int	i;
 
 	i = 0;
-	if (!stack || !*stack)
+	if (!stack_1 || !*stack_1)
 		return ;
-	if (ft_lstsize(*stack) < (int)n * 2)
-		return (revrotate_i(stack, ft_lstsize(*stack) - n, ab));
+	if (ft_lstsize(*stack_1) < (int)n * 2 && ab != 'r')
+		return (revrotate_i(stack_1, ft_lstsize(*stack_1) - n, ab));
 	while (i < n)
 	{
 		if (ab == 'a')
-			ra(stack);
-		else
-			rb(stack);
+			ra(stack_1);
+		else if (ab == 'b')
+			rb(stack_1);
+		else if (ab == 'r')
+			rr(stack_1, stack_2);
 		i++;
 	}
 	return ;
